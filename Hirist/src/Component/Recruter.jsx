06403c62a3cb24fad18da98bbf20e95060c89edc
NOOR <noormuhammed14@gmail.com>
2021-07-23 15:08:26 +0530

    updated

---
+++ Hirist/src/Component/Recruter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Foter from './Foter'
+
+const Recruter = () => {
+    return (
+        <>
+        <div className="container">
+            <Header/>  
+            <Main/>
+           <Foter/>
+        </div>
+        </>
+    )
+}
+export default Recruter
