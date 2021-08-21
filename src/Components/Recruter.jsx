bf795e40af66e31f40e1b7aa54c432d2b59b08d4
Amarjeet Singh <73219811+ajkarnawal0001@users.noreply.github.com>
2021-08-21 15:14:57 +0530

    recruiter

---
+++ src/Components/Recruter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+const Recruter = () => {
+    return (
+        <>
+            <div className="container">
+                <Header />
+                <Main />
+                <Footer />
+            </div>
+        </>
+    )
+}
+export default Recruter
